2a0c8ed82025988aa468fb8ed1cbcea6e272d3e0
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2025-02-06 07:05:02 -0300

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to hoarder-graalvm
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/hoarder_graalvm/core_test.clj
@@ -0,0 +1,7 @@
+(ns hoarder-graalvm.core-test
+  (:require [clojure.test :refer :all]
+            [hoarder-graalvm.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
