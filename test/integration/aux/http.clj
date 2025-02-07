ac78d81aa347130fad264a4062e01f90b4afba38
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2025-02-07 20:59:42 -0300

    add tests for fetch file operation

--- test/integration/aux/http.clj
+++ test/integration/aux/http.clj
@@ -11,3 +11,11 @@
                                                  :body (json/encode {:file file}))]
     {:status status
      :body   (json/decode body true)}))
+
+(defn fetch-file
+  [file-id
+   service-fn]
+  (let [{:keys [body status]} (test/response-for service-fn
+                                                 :get (str "/api/files/" file-id))]
+    {:status #p status
+     :body   (json/decode #p body true)}))
--- test/integration/file_test.clj
+++ test/integration/file_test.clj
@@ -20,3 +20,20 @@
                    :status 200}
                   (aux.http/create-file! {:file {:name fixtures.file/file-name}} service-fn))))
     (ig/halt! system)))
+
+(s/deftest fetch-file-test
+  (let [system (aux.components/start-system!)
+        service-fn (-> system ::component.service/service :io.pedestal.http/service-fn)
+        file-id (-> (aux.http/create-file! {:file {:name fixtures.file/file-name}} service-fn)
+                    :body
+                    :file
+                    :id)]
+
+    (testing "That we can fetch a file entity"
+      (is (match? {:body   {:file {:id           clj-uuid/uuid-string?
+                                   :name         fixtures.file/file-name
+                                   :access-count 0
+                                   :created-at   string?}}
+                   :status 200}
+                  (aux.http/fetch-file file-id service-fn))))
+    (ig/halt! system)))
